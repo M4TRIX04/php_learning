38ec66d7e6d6976d65e767c168131a51c82e96c7
coderpark <worldofliu@gmail.com>
2015-12-03 21:59:01 +0800

    go on

--- array.php
+++ array.php
@@ -1,7 +1,24 @@
 <?php
-	$products = array( 'Tires', 'Oil', 'Spark Plugs');
+	$products = array('Tires', 'Oil', 'Spark Plugs');
 	$products[3] = 'Fuses';
 	#$out =  `ls -la`;
 	#echo '<pre> '.$out.'</pre>'
-	
+
+	foreach ($products as $current){
+		echo $current."<br>";
+	}
+
+	$prices = array( 'Tires' => 100, 'Oil' => 10, 'Spark Plugs' => 4 );
+
+	while ($element = each($prices)) {
+	echo $element['key'];
+	echo " - ";
+	echo $element['value'];
+	echo "<br />";
+	}
+
+	reset ($prices);
+	while (list($product, $price) = each($prices)){
+		echo "$product - $price - $prices<br />";
+	}
 ?>
